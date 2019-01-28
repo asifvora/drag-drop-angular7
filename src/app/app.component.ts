82b96dab17c55d7b628da7dfcc92e61b9ec72841
asif <asif.vora@multidots.com>
2019-01-28 18:23:05 +0530

    (feat): Drag and drop
    Drag & drop implements with tree.
    
    #version:0.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,61 +10,45 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 export class AppComponent {
   title = 'Drag & Drop in Angular 7';
   website = 'https://samorgill.com';
+  isSubDrop = false;
+  LIST_IDS = [];
 
   todos = [
     {
+      Id: 1,
       name: 'Angular',
       category: 'Web Development',
       nodes: [{ name: 'Angular-1' }, { name: 'Angular-2' }, { name: 'Angular-3' }]
     },
     {
+      Id: 2,
       name: 'Flexbox',
       category: 'Web Development',
       nodes: [{ name: 'Flexbox-1' }, { name: 'Flexbox-2' }, { name: 'Flexbox-3' }]
-    },
-    // {
-    //   name: 'iOS',
-    //   category: 'App Development',
-    //   nodes: [{ name: 'iOS-1' }, { name: 'iOS-2' }, { name: 'iOS-3' }]
-    // },
-    // {
-    //   name: 'Java',
-    //   category: 'Software development',
-    //   nodes: [{ name: 'Java-1' }, { name: 'Java-2' }, { name: 'Java-3' }]
-    // }
+    }
   ];
 
   onDrop(event: CdkDragDrop<string[]>) {
-    console.log('event', event)
+    console.log('event onDrop :: ', event)
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  }
+
+  onDropSub(event: CdkDragDrop<string[]>) {
+    console.log('****** onDropSub *******')
     if (event.previousContainer === event.container) {
-      console.log('If ***')
-      moveItemInArray(event.container.data,
-        event.previousIndex,
-        event.currentIndex);
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      console.log('else ***')
-      transferArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex, event.currentIndex);
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
     }
   }
 
-  onDropSub(event: CdkDragDrop<string[]>, index) {
-    console.log('****** onDropSub *******', index)
-    console.log(' event', event)
-    if (event.previousContainer === event.container) {
-      console.log('If ***')
-      // moveItemInArray(event.container.data[index]['nodes'],
-      moveItemInArray(event.container.data,
-        event.previousIndex,
-        event.currentIndex);
-    } else {
-      console.log('else ***')
-      transferArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex, event.currentIndex);
+  addId(i) {
+    const id = 'cdk-drop-list-' + i;
+    if (this.LIST_IDS.indexOf(id) === -1) {
+      this.LIST_IDS.push(id);
     }
-  }
 
+    return id;
+  }
 
 }
