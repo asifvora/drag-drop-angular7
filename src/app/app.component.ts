9cfb81e7189acb0d14c1762af41415e5683d4904
asif <asif.vora@multidots.com>
2019-01-29 14:30:03 +0530

    (feat): Drag and drop
    Multilevel tree render.
    
    #version:0.0.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Drag & Drop in Angular 7';
   website = 'https://samorgill.com';
   isSubDrop = false;
   LIST_IDS = [];
+  array = ["<ul>"];
 
   todos = [
     {
@@ -19,9 +21,9 @@ export class AppComponent {
       name: 'Angular',
       category: 'Web Development',
       nodes: [
-        { name: 'Angular-1', nodes: [{ name: 'NG-1' }, { name: 'NG-1.1' }] },
-        { name: 'Angular-2', nodes: [{ name: 'NG-2' }, { name: 'NG-2.1' }] },
-        { name: 'Angular-3', nodes: [{ name: 'NG-3' }, { name: 'NG-3.1' }] }
+        { name: 'Angular-1', nodes: [{ name: 'Angular-1.1', nodes: [] }, { name: 'Angular-1.2', nodes: [{ name: 'Angular-1.2.1', nodes: [] }] }] },
+        { name: 'Angular-2', nodes: [] },
+        { name: 'Angular-3', nodes: [] }
       ]
     },
     {
@@ -29,12 +31,80 @@ export class AppComponent {
       name: 'Flexbox',
       category: 'Web Development',
       nodes: [
-        { name: 'Flexbox-1', nodes: [{ name: 'FB-1' }] },
-        { name: 'Flexbox-2', nodes: [{ name: 'FB-2' }] },
-        { name: 'Flexbox-3', nodes: [{ name: 'FB-3' }] }
+        { name: 'Flexbox-1', nodes: [{ name: 'fb-1.1', nodes: [{ name: 'fb-1.1.1', nodes: [{ name: 'fb-1.1.1.1', nodes: [] }] }] }] },
+        { name: 'Flexbox-2', nodes: [] },
+        { name: 'Flexbox-3', nodes: [] }
       ]
     }
   ];
+
+
+  ngOnInit() {
+    this.printList(this.todos);
+    this.array.push("</ul>");
+    $("#list").html(this.array.join(""));
+    console.log(' this.array')
+  }
+
+  printList(items) {
+    // console.log('type ::', items)
+    switch ($.type(items)) {
+      case "object":
+        this.getChildren(items);
+        break;
+      case "string":
+        this.array.push("<li>" + items + "</li>");
+        break;
+      case "array":
+        this.printArray(items);
+        break;
+    }
+  }
+
+  getChildren(parent) {
+    if ($.type(parent) === "array" && parent.length > 0) {
+      this.array.push("<ul>");
+    }
+
+    if ($.type(parent) === "array" && parent.length > 0) {
+      for (let child in parent) {
+        if (parent[child].name) {
+          this.array.push("<li>" + parent[child].name);
+        }
+        if (parent[child].nodes.length > 0) {
+          this.array.push("<ul >");
+        }
+        this.printArray(parent[child].nodes);
+        if (parent[child].nodes.length > 0) {
+          this.array.push("</ul >");
+        }
+        if (parent[child].name) {
+          this.array.push("</li>");
+        }
+      }
+    } else {
+      for (let child in parent) {
+        this.printList(parent[child]);
+      }
+    }
+
+    if ($.type(parent) === "array" && parent.length > 0) {
+      this.array.push("</ul>");
+    }
+  }
+
+  printArray(myArray) {
+    const length = myArray.length;
+    for (let i = 0; i < length; i++) {
+      if (myArray[i].nodes.length > 0) {
+        this.array.push("<li><b>" + myArray[i].name + "</b>");
+      } else {
+        this.array.push("<li>" + myArray[i].name);
+      }
+      this.getChildren(myArray[i].nodes);
+      this.array.push("</li>");
+    }
+  }
 
   onDrop(event: CdkDragDrop<string[]>) {
     console.log('event onDrop :: ', event)
@@ -58,5 +128,18 @@ export class AppComponent {
 
     return id;
   }
+
+  findObjectByLabel(obj, key) {
+    console.log('obj, key', obj, key)
+    if (obj.key === key) { return obj; }
+    for (var i in obj) {
+      if (obj.hasOwnProperty(i)) {
+        var foundLabel = this.findObjectByLabel(obj[i], key);
+        if (foundLabel) { return foundLabel; }
+      }
+    }
+    return null;
+  };
+
 
 }
